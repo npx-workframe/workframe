77256b33afa39b8f412e9e362e5272cdba040da5
Architectonic <47732604+architectonic@users.noreply.github.com>
2026-07-29 02:25:33 -0300

    fix(origin): include start in all help aliases

--- packages/workframe/bin/workframe-cli.js
+++ packages/workframe/bin/workframe-cli.js
@@ -8,7 +8,7 @@ if (command === 'start') {
   await runOriginStart(args.slice(args.indexOf('start') + 1));
 } else {
   await import('./workframe.js');
-  if (command === 'help') {
+  if (command === 'help' || args.some((arg) => ['--help', '-h'].includes(arg))) {
     console.log('\nAdditional command:\n  start      Begin the experimental plan-only formation preflight.');
   }
 }
